c0cecb0e3b2fb00e1afaaf5d95e081552eb8ae3c
Crem2y <31000852+Crem2y@users.noreply.github.com>
2026-07-30 15:46:32 +0900

    Update uart_log of southbridge

--- pico_southbridge/driver/uart_log/uart_log.c
+++ pico_southbridge/driver/uart_log/uart_log.c
@@ -1,13 +1,10 @@
 #include "uart_log.h"
 
 static uart_inst_t* _uart;
-static int _tx_pin;
-static int _rx_pin;
-static int _baudrate;
 
-volatile char rx_buf[64];
-volatile int rx_idx = 0;
+enum log_level log_lvl;
 
+/*
 int _write(int fd, const void *buf, size_t count) {
   const char *cbuf = (const char *)buf;
   for (size_t i = 0; i < count; ++i) {
@@ -15,36 +12,73 @@ int _write(int fd, const void *buf, size_t count) {
   }
   return count;
 }
-
-void on_uart_rx(void) {
-  while (uart_is_readable(_uart)) {
-    char c = uart_getc(_uart);
-    //uart_putc_raw(_uart, c); // loopback test
-
-    rx_buf[rx_idx++] = c;
-    if (rx_idx >= sizeof(rx_buf)) rx_idx = 0;
-  }
-}
+*/
 
 void uartLog_init(uart_inst_t* uart, int tx_pin, int rx_pin, int baudrate) {
   _uart = uart;
-  _tx_pin = tx_pin;
-  _rx_pin = rx_pin;
-  _baudrate = baudrate;
+  uart_bridge_init(uart, tx_pin, rx_pin, baudrate);
 
-  uart_init(_uart, _baudrate);
-  gpio_set_function(_tx_pin, GPIO_FUNC_UART);
-  gpio_set_function(_rx_pin, GPIO_FUNC_UART);
+  uart_bridge_enable_irq();
 
-  irq_set_exclusive_handler(UART0_IRQ, on_uart_rx);
-  irq_set_enabled(UART0_IRQ, true);
-
-  uart_set_irq_enables(_uart, true, false); // RX: true, TX: false
-
-  setlocale(LC_ALL,"");
+  uartLog_set_level(LOG_DEFAULT_LEVEL);
 }
 
-void uartLog_set_baudrate(int baudrate) {
-  _baudrate = baudrate;
-  uart_init(_uart, _baudrate);
+void uartLog_set_level(enum log_level level) {
+  log_lvl = level;
+}
+
+void uartLog_print(const char *s) {
+  size_t len = 0;
+  while (s[len]) {
+    len++;
+  }
+  uart_bridge_write(s, len);
+}
+
+int uartLog_printf(enum log_level level, const char *format, ...) {
+  if(level > log_lvl) return 0;
+
+#if LOG_PRINT_WITH_LEVEL
+  switch(level) {
+    case LOG_ERROR:
+      uartLog_print("[ERR]");
+      break;
+    case LOG_WARN:
+      uartLog_print("[WARN]");
+      break;
+    case LOG_INFO:
+      uartLog_print("[INFO]");
+      break;
+    case LOG_DEBUG:
+      uartLog_print("[DBG]");
+      break;
+    case LOG_TRACE:
+      uartLog_print("[TRACE]");
+      break;
+  }
+#endif
+
+  va_list args;
+  va_start(args, format);
+
+  int result = uartLog_vprintf(format, args);
+
+  return result;
+}
+
+int uartLog_vprintf(const char *format, va_list args) {
+  char string_buf[LOG_PRINTF_BUFFER_SIZE];
+
+  int len = vsnprintf(
+    string_buf,
+    sizeof(string_buf),
+    format,
+    args
+  );
+
+  if (len >= 0) {
+    uartLog_print(string_buf);
+  }
+
+  return len;
 }
--- pico_southbridge/driver/uart_log/uart_log.h
+++ pico_southbridge/driver/uart_log/uart_log.h
@@ -1,22 +1,40 @@
 #pragma once
 
 #include <stdio.h>
-// for using unicode
-#include <wchar.h>
-#include <locale.h>
+#include <string.h>
 
-#include "pico/stdlib.h"
-#include "hardware/uart.h"
+#include "uart_bridge.h"
 
-#define LOG_PRINTF(fmt, ...) wprintf(L##fmt, ##__VA_ARGS__)
+enum log_level {
+  LOG_ERROR = 0,
+  LOG_WARN,
+  LOG_INFO,
+  LOG_DEBUG,
+  LOG_TRACE,
+};
+
+#define LOG_DEFAULT_LEVEL LOG_INFO
+#define LOG_PRINT_WITH_LEVEL 1
+#define LOG_PRINTF_BUFFER_SIZE 256
+
+//#define LOG_PRINTF(...) printf(__VA_ARGS__)
+#define LOG_PRINTF(...) uartLog_printf(LOG_DEFAULT_LEVEL, __VA_ARGS__)
+#define LOGE(...) uartLog_printf(LOG_ERROR, __VA_ARGS__)
+#define LOGW(...) uartLog_printf(LOG_WARN,  __VA_ARGS__)
+#define LOGI(...) uartLog_printf(LOG_INFO,  __VA_ARGS__)
+#define LOGD(...) uartLog_printf(LOG_DEBUG, __VA_ARGS__)
+#define LOGT(...) uartLog_printf(LOG_TRACE, __VA_ARGS__)
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 void uartLog_init(uart_inst_t* _uart, int tx_pin, int rx_pin, int baudrate);
-void uartLog_set_baudrate(int baudrate);
-void on_uart_rx(void);
+
+void uartLog_set_level(enum log_level level);
+void uartLog_print(const char *s);
+int uartLog_printf(enum log_level level, const char* format, ...);
+int uartLog_vprintf(const char *format, va_list args);
 
 #ifdef __cplusplus
 }
