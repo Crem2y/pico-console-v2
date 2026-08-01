c1c980834d3986daf64e567b8474d31aa4cb0c3f
Crem2y <31000852+Crem2y@users.noreply.github.com>
2026-08-01 22:20:31 +0900

    Convert build date and time to BCD format (SB v1.0.02)

--- pico_console_v2/include/v2_hw_def.h
+++ pico_console_v2/include/v2_hw_def.h
@@ -23,6 +23,14 @@
 #define TIME_MM ((__TIME__ [3] - '0') * 10 + (__TIME__ [4] - '0'))
 #define TIME_SS ((__TIME__ [6] - '0') * 10 + (__TIME__ [7] - '0'))
 
+// build date (BCD)
+#define DATE_YY_BCD ((DATE_YY / 10) << 4 | (DATE_YY % 10))
+#define DATE_MM_BCD ((DATE_MM / 10) << 4 | (DATE_MM % 10))
+#define DATE_DD_BCD ((DATE_DD / 10) << 4 | (DATE_DD % 10))
+#define TIME_HH_BCD ((TIME_HH / 10) << 4 | (TIME_HH % 10))
+#define TIME_MM_BCD ((TIME_MM / 10) << 4 | (TIME_MM % 10))
+#define TIME_SS_BCD ((TIME_SS / 10) << 4 | (TIME_SS % 10))
+
 // pin definitions
 #define PIN_LOG_TX      0
 #define PIN_LOG_RX      1
